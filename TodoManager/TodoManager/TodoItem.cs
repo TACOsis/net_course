e798e797ca05e0dc7991fc8597e25adb2ce377e0
VRyzhov <VRyzhov@beeline.kz>
2026-09-18 01:38:17 +0500

    Добавил реализацию добавления задачи TodoService -AddAsync. Реализовал конструктор и передачу в него значений через CreateAsync

--- TodoManager/TodoManager/TodoItem.cs
+++ TodoManager/TodoManager/TodoItem.cs
@@ -4,8 +4,8 @@ public class TodoItem
 {
     public Guid? Id { get; set; }
     public string? Title { get; set; }
-    public TodoStatus? Status { get; set; }
-    public DateTimeOffset? CreatedAt { get; set; }
-    public DateTimeOffset? DueAt { get; set; }
-    public DateTimeOffset? CompletedAt { get; set; }
+    public TodoStatus? Status { get; set; } = TodoStatus.New;
+    public DateTimeOffset? CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateOnly? DueAt { get; set; }
+    public DateTimeOffset? CompletedAt { get; set; } =  DateTimeOffset.UtcNow;
 }
--- TodoManager/TodoManager/TodoService.cs
+++ TodoManager/TodoManager/TodoService.cs
@@ -2,20 +2,47 @@ namespace TodoManager;
 
 public class TodoService
 {
-    private readonly ITodoStorage _todoStorage;
+    private readonly ITodoStorage _storage;
+    private readonly List<TodoItem> _items;
 
-    public TodoService(ITodoStorage todoStorage)
+    private TodoService(ITodoStorage storage, List<TodoItem> items)
     {
-        _todoStorage = todoStorage;
+        _storage = storage;
+        _items = items;
+    }
+
+    public static async Task<TodoService> CreateAsync(ITodoStorage storage)
+    {
+        var items = await storage.LoadAsync();
+        return new TodoService(storage, items);
     }
     
     public Task<List<TodoItem>> LoadAsync(string? path = null)
     {
-        return _todoStorage.LoadAsync(path);
+        return _storage.LoadAsync(path);
     }
 
     public async Task SaveAsync(IReadOnlyList<TodoItem> items)
     {
-        await _todoStorage.SaveAsync(items);
+        await _storage.SaveAsync(items);
+    }
+
+    public async Task<TodoItem> AddAsync(string? title, DateOnly? dueDate)
+    {
+        if (title == null || string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title cannot be null or whitespace.", nameof(title));
+
+        if (dueDate == null) throw new ArgumentException("Due Date cannot be null.", nameof(dueDate));
+
+        var item = new TodoItem()
+        {
+            Id = Guid.NewGuid(),
+            Title = title,
+            DueAt = dueDate,
+        };
+        
+        _items.Add(item);
+        await _storage.SaveAsync(_items);
+        
+        return item;
     }
 }
