79f489e77cea64b7d6562fc78dffb887445362bb
VRyzhov <VRyzhov@beeline.kz>
2026-09-18 10:15:09 +0500

    коммит недостающихся файлов

---
+++ TodoManager/TodoManager/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace  TodoManager;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        var path = "/Users/vryzhov/dotnet_course/TodoManager/TodoManager/obj/Debug/todoList.json";
+        
+        var jsonTodoStorage = new JsonTodoStorage(path);
+        var todoService = await TodoService.CreateAsync(jsonTodoStorage);
+
+        await todoService.AddAsync("Test", new DateOnly(2026, 9, 12));
+    }
+}
---
+++ week_2/week_2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace week_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var VARIABLE in Numbers())
+        {
+            Console.WriteLine($"принимаю {VARIABLE}");
+        }
+    }
+    
+    static IEnumerable<int> Numbers()
+    {
+        Console.WriteLine("отдаю 1");
+        yield return 1;
+        Console.WriteLine("отдаю 2");
+        yield return 2;
+    }
+}
